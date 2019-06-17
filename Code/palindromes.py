0a199accc37d39690b178e006edc9e72dba8de19
Ricardo Rodriguez <rr092293@gmail.com>
2019-06-16 17:46:36 -0700

    completed palindromes functions

--- Code/palindromes.py
+++ Code/palindromes.py
@@ -13,22 +13,85 @@ def is_palindrome(text):
     # implement is_palindrome_iterative and is_palindrome_recursive below, then
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
-    return is_palindrome_iterative(text)
-    # return is_palindrome_recursive(text)
+    #return is_palindrome_iterative(text)
+    return is_palindrome_recursive(text)
 
 
 def is_palindrome_iterative(text):
-    # TODO: implement the is_palindrome function iteratively here
-    pass
-    # once implemented, change is_palindrome to call is_palindrome_iterative
-    # to verify that your iterative implementation passes all tests
+
+    # Set left index to 0, set right index to the last index of the given text
+    left = 0
+    right = len(text) - 1
+
+    while left < right:
+        # Set left_char the character at the left index in text, Set right_char the character at the right index in text
+        left_char = text[left]
+        right_char = text[right]
+
+        # Check if left_char is an alphabet character
+        if not left_char.isalpha():
+            # If it's not, we don't care for it so we increase left index by 1
+            left += 1
+            # continue on to the next iteration of the loop
+            continue
+
+        # Check if right_char is an alphabet character
+        if not right_char.isalpha():
+            # If it's not, we don't care for it so we increase right index by 1
+            right -= 1
+            # continue on to the next iteration of the loop
+            continue
+
+        # Compare the two letters to see if they are the same
+        if left_char.lower() == right_char.lower():
+            # If they are the same, increase/decrease the indexes
+            left += 1
+            right -= 1
+            continue
+        else:
+            # If they're not, return false since this text is not a palindrome
+            return False
+
+    return True
+
 
 
 def is_palindrome_recursive(text, left=None, right=None):
-    # TODO: implement the is_palindrome function recursively here
-    pass
-    # once implemented, change is_palindrome to call is_palindrome_recursive
-    # to verify that your iterative implementation passes all tests
+    # Set left index to 0, set right index to the last index of the given text
+    if left is None:
+        left = 0
+        right = len(text) - 1
+
+    if left >= right:
+        return True
+
+    # Set left_char the character at the left index in text, Set right_char the character at the right index in text
+    left_char = text[left]
+    right_char = text[right]
+
+    # Check if left_char is an alphabet character
+    if not left_char.isalpha():
+        # If it's not, we don't care for it so we increase left index by 1
+        left += 1
+        return is_palindrome_recursive(text, left, right)
+
+    # Check if right_char is an alphabet character
+    if not right_char.isalpha():
+        # If it's not, we don't care for it so we increase right index by 1
+        right -= 1
+        return is_palindrome_recursive(text, left, right)
+
+    # Compare the two letters to see if they are the same
+    if left_char.lower() == right_char.lower():
+        # If they are the same, increase/decrease the indexes
+        left += 1
+        right -= 1
+        return is_palindrome_recursive(text, left, right)
+    else:
+        # If they're not, return false since this text is not a palindrome
+        return False
+
+
 
 
 def main():
